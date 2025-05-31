8051614f96b60225c917337968cc478a98f0e51c
Sobhan Shahnavaz <102673272+SobhanShahnavaz@users.noreply.github.com>
2025-05-31 14:38:05 +0330

    Update Q1.py
    added comments

--- Q1.py
+++ Q1.py
@@ -1,8 +1,9 @@
 #Sobhan
+#DARYAFT
 Y = int(input("tool ra vared konid:"))
 X = int(input("Arz ra vared konid:"))
-
+#MOHASEBE
 MO = (X*2) + (Y*2)
 MA = X*Y
-
+#NAMAYESH
 print(f"Mohit:{MO} , MASAHAT:{MA}")
